b90211e1d2862db9f35c5d139e7475d4bd290b9a
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-25 19:42:40 +0400

    Update amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -16,6 +16,14 @@ def get_amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
+@app_views.route('/amenities', methods=['GET'],
+                 strict_slashes=False)
+def get_all_amenity():
+    """Get amenity by id"""
+    amenities = storage.all("Amenity")
+    return jsonify([amenity.to_dict() for amenity in amenities.values()])
+
+
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_amenity(amenity_id):
@@ -28,7 +36,7 @@ def delete_amenity(amenity_id):
     return {}, 200
 
 
-@app_views.route('/amenities', methods=['GET'],
+@app_views.route('/amenities', methods=['POST'],
                  strict_slashes=False)
 def post_amenity():
     """Post a new amenity"""
