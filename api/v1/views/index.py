3a8f9e9e68df526a5585800418c0cbafa11a4d30
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-25 18:42:12 +0400

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -21,11 +21,11 @@ def status():
 def stats():
     '''Doc for stats'''
     stats = {
-        "amenities": storage.count(Amenity()),
-        "cities": storage.count(City()),
-        "places": storage.count(Place()),
-        "reviews": storage.count(Review()),
-        "states": storage.count(State()),
-        "users": storage.count(User())
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
     }
     return jsonify(stats)
