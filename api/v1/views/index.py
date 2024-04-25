1865f0368bb59aecb27d192e23f52bec67ff6bc6
Ravan Asadov <45626962+ravanasad@users.noreply.github.com>
2024-04-25 18:38:26 +0400

    Updated index.py
    Created stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
 '''Doc for status'''
 from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
 @app_views.route('/status')
 def status():
     '''Doc for status'''
     return {"status": "OK"}
+
+
+@app_views.route('/stats')
+def stats():
+    '''Doc for stats'''
+    stats = {
+        "amenities": storage.count(Amenity()),
+        "cities": storage.count(City()),
+        "places": storage.count(Place()),
+        "reviews": storage.count(Review()),
+        "states": storage.count(State()),
+        "users": storage.count(User())
+    }
+    return jsonify(stats)
