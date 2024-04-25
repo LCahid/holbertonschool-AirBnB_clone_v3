091e8fea886a46ae90660a6f55bfab7b6abeb4ef
Cahid <mr.cebizade@gmail.com>
2024-04-25 18:21:29 +0400

    pycodestyle problem fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+'''Doc for api'''
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-'''Doc for api'''
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
